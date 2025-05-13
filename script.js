add0f54859f9c928440fef8031635cfb0a3dc76f
Nicolas Personnaz <117669784+NicolasPersonnaz@users.noreply.github.com>
2025-05-13 14:27:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.fa-bars');
-    const navUl = document.querySelector('nav ul');
-
-    menuIcon.addEventListener('click', function() {
-        navUl.classList.toggle('show');
-    });
-});
